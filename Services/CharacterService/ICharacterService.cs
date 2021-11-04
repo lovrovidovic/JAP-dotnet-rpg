6c72c7494c73eee6173b8a151d200b7632c1d595
lovrovidovic <lovrovidovic6@gmail.com>
2021-11-04 13:06:19 +0100

    finished section 2

---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,20 @@
+﻿using dotnet_rpg.Dtos.Character;
+using dotnet_rpg.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters();
+
+        Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
+
+        Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
+
+        Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updareCharacter);
+
+        Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id);
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,8 @@ namespace dotnet_rpg
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnet_rpg", Version = "v1" });
             });
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<ICharacterService, CharacterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
